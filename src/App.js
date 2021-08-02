90ac46e685d1a5c8725e5ae008a16ea451b85be6
gordongecco <delldani001@gmail.com>
2021-08-02 17:16:42 +0200

    ref

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-
 function App() {
-
-  const onClick = ()=>{
+  const onClick = () => {
     const str = window.location.href;
-    let token = '';
+    let token = "";
     let isToken = false;
-   console.log(str);
-   for(let i = 0; i < str.length; i++){
-    if(str[i] === '&'){
-      break;
+    console.log(str);
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === "&") {
+        break;
+      }
+      if (isToken) {
+        token = token + str[i];
+      }
+
+      if (str[i] === "=") {
+        isToken = true;
+      }
     }
-if(isToken){
-  token = token + str[i];
-}
 
-    if(str[i] === '='){
-      isToken = true;
-    }
+    const base = "https://www.googleapis.com/youtube/v3/channels?";
+    const tokenStr = `access_token=${token}&`;
+    const part = "part=snippet,contentDetails,statistics,status";
+    const URL = `${base}${tokenStr}${part}&mine=true&key=AIzaSyBxocfML3lbHXfX5LhHLeAH4ZBrMFOaut8`;
 
-   };
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
 
-
-
-   const URL = `https://www.googleapis.com/youtube/v3/channels?access_token=${token}&part=snippet,contentDetails&mine=true&key=AIzaSyBxocfML3lbHXfX5LhHLeAH4ZBrMFOaut8`;
-
-   fetch(URL)
-  .then(response => response.json())
-  .then(data => console.log(data));
-  }
-  
-  return <div className="App">
-
-<button onClick={onClick}>trigger</button>
-<br/>
-<a href="https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/youtube.readonly&redirect_uri=https://localhost:3000&response_type=token&client_id=510827311023-etegid0lap83anh9ie0ikvklh7l0t4ue.apps.googleusercontent.com">Visit </a>
-
-  </div>;
+  return (
+    <div className="App">
+      <button onClick={onClick}>trigger</button>
+      <br />
+      <a href="https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/youtube.readonly&redirect_uri=https://localhost:3000&response_type=token&client_id=510827311023-etegid0lap83anh9ie0ikvklh7l0t4ue.apps.googleusercontent.com">
+        Visit{" "}
+      </a>
+    </div>
+  );
 }
 
 export default App;
